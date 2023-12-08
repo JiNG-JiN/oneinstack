9f13943e0753247b5fba0e8bc858dd7050423b8d
Jing <jing.jin.jp@gmail.com>
2023-12-08 12:32:11 +0800

    update mirror link and support webp.

--- include/php-8.2.sh
+++ include/php-8.2.sh
@@ -101,7 +101,7 @@ Install_PHP82() {
     --with-config-file-scan-dir=${php_install_dir}/etc/php.d \
     --with-apxs2=${apache_install_dir}/bin/apxs ${phpcache_arg} --disable-fileinfo \
     --enable-mysqlnd --with-mysqli=mysqlnd --with-pdo-mysql=mysqlnd \
-    --with-iconv=/usr/local --with-freetype --with-jpeg --with-zlib \
+    --with-iconv=/usr/local --with-freetype --with-jpeg --with-zlib --with-webp \
     --enable-xml --disable-rpath --enable-bcmath --enable-shmop --enable-exif \
     --enable-sysvsem ${php82_with_curl} --enable-mbregex \
     --enable-mbstring --with-password-argon2 --with-sodium=/usr/local --enable-gd ${php82_with_openssl} \
@@ -112,7 +112,7 @@ Install_PHP82() {
     --with-config-file-scan-dir=${php_install_dir}/etc/php.d \
     --with-fpm-user=${run_user} --with-fpm-group=${run_group} --enable-fpm ${phpcache_arg} --disable-fileinfo \
     --enable-mysqlnd --with-mysqli=mysqlnd --with-pdo-mysql=mysqlnd \
-    --with-iconv=/usr/local --with-freetype --with-jpeg --with-zlib \
+    --with-iconv=/usr/local --with-freetype --with-jpeg --with-zlib  --with-webp \
     --enable-xml --disable-rpath --enable-bcmath --enable-shmop --enable-exif \
     --enable-sysvsem ${php82_with_curl} --enable-mbregex \
     --enable-mbstring --with-password-argon2 --with-sodium=/usr/local --enable-gd ${php82_with_openssl} \
