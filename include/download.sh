46857d84385ba6b0659931a2659aa3bf15beaca8
Jing <jing.jin.jp@gmail.com>
2023-12-11 14:08:25 +0800

    Update src url

--- include/download.sh
+++ include/download.sh
@@ -9,9 +9,17 @@
 #       https://github.com/oneinstack/oneinstack
 
 Download_src() {
-  [ -s "${src_url##*/}" ] && echo "[${CMSG}${src_url##*/}${CEND}] found" || { wget --limit-rate=100M --tries=6 -c --no-check-certificate ${src_url}; sleep 1; }
-  if [ ! -e "${src_url##*/}" ]; then
-    echo "${CFAILURE}Auto download failed! You can manually download ${src_url} into the oneinstack/src directory.${CEND}"
+  local file_name=${src_url##*/}
+  
+  # 特殊处理 GitHub 链接
+  if [[ "$src_url" == *github.com/P-H-C/phc-winner-argon2/archive/refs/tags/* ]]; then
+    argon2_ver=$(echo $src_url | grep -oP '(?<=tags/)[^/.]+')
+    file_name="phc-winner-argon2-${argon2_ver}.tar.gz"
+  fi
+
+  [ -s "$file_name" ] && echo "[${CMSG}${file_name}${CEND}] found" || { wget --limit-rate=100M --tries=6 -c --no-check-certificate -O "$file_name" "$src_url"; sleep 1; }
+  if [ ! -e "$file_name" ]; then
+    echo "${CFAILURE}Auto download failed! You can manually download $src_url into the oneinstack/src directory.${CEND}"
     kill -9 $$; exit 1;
   fi
 }
